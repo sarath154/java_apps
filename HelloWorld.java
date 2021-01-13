e2153f24a51746a2d94759c1d94a4d209d49daca
Sarath <sarathreddy1712@gmail.com>
2021-01-13 02:48:36 +0000

    pushed to trigger Webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!--Poll SCM");
+    System.out.println("Hello world!--WebHook Trigger");
   }
 }
